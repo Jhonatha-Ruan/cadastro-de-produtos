ff5cf274b8e7acb1c940b0b3396710d066ddaf3d
Jhonatha Ruan <jhonatanruan95@gmail.com>
2022-08-23 20:37:12 -0300

    update

--- src/app/home/carteira.ts
+++ src/app/home/carteira.ts
@@ -4,12 +4,20 @@ export class Carteira{
     caixa: string;
     descricao: string;
     data_valor: string;
-
     nome: string;
     quantidade: number;
     telefone: number;
+
+    //Fornecedores
+    fornecedor: string;
+    rua: string;
+    bairro: string;
+    cidade: string;
+    cep: number;
+    estado: string;
+
     
-    constructor(id?: number, valor?: number, caixa?:string, descricao?: string, data_valor?: string, nome?: string, quantidade?: number, telefone?: number){
+    constructor(id?: number, valor?: number, caixa?:string, descricao?: string, data_valor?: string, nome?: string, quantidade?: number, telefone?: number, rua?: string, bairro?: string, cidade?: string, cep?: number, estado?: string, fornecedor?: string){
         this.id = id;
         this.valor = valor;
         this.caixa = caixa;
@@ -19,6 +27,14 @@ export class Carteira{
         this.nome = nome;
         this.quantidade = quantidade;
         this.telefone = telefone;
+
+        //Fornecedores
+        this.fornecedor = fornecedor;
+        this.rua = rua;
+        this.bairro = bairro;
+        this.cidade = cidade;
+        this.cep = cep;
+        this.estado = estado;
         
     }
 }
